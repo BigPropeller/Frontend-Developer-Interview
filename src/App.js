3cfaf95c873ea46ccf7ffd144c39f45b51e21edf
Yadiel Arroyo <yadielar@gmail.com>
2018-11-12 22:36:09 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { Wrapper } from './components/Wrapper';
+import CommentsContainer from './containers/CommentsContainer';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper className="App">
+        <CommentsContainer />
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/CommentsContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addComment } from '../actions';
+import { getComments } from '../reducers';
+import { Pane } from '../components/Pane';
+import { CommentsList } from '../components/CommentsList';
+import { CommentEditor } from '../components/CommentEditor';
+
+
+const CommentsContainer = ({ comments, addComment }) => (
+  <>
+    <h1>Chat</h1>
+    <Pane>
+      <CommentsList comments={comments} />
+      <CommentEditor onSubmit={comment => addComment(comment)} />
+    </Pane>
+  </>
+);
+
+CommentsContainer.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    message: PropTypes.string.isRequired,
+    mentions: PropTypes.array.isRequired,
+  })).isRequired,
+  addComment: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  comments: getComments(state),
+});
+
+export default connect(
+  mapStateToProps,
+  { addComment }
+)(CommentsContainer);
